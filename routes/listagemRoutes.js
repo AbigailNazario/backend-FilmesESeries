58b29c37f0af35bab9a51d391ac27103c5fb865d
AbigailNazario <136490258+AbigailNazario@users.noreply.github.com>
2026-04-18 14:54:46 -0300

    update de caminho no routes

--- routes/listagemRoutes.js
+++ routes/listagemRoutes.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const ListagemController = require('../controllers/listagemController');
+const listagemController = require('../controllers/listagemController');
 
 router.get('/', listagemController.listEntries);
 router.get('/:id', listagemController.getEntryById);
-router.get('/:titulo', listagemController.getEntryByName);
+router.get('/nome/:titulo', listagemController.getEntryByName);
 router.post('/', listagemController.createEntry);
 router.put('/:id', listagemController.updateEntry);
 router.delete('/:id', listagemController.deleteEntry);
